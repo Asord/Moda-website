fff35f28e9a6fb78c04fdc766e37b3a39fbfe1af
Killian P <killian.perche@etu.udamail.fr>
2017-04-10 17:42:50 +0200

    Simplification du formulaire de connection

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 
 <?php
+    require_once "utility.php";
 	require_once "pageManager.php";
     require_once "template.php";
 
@@ -12,44 +13,42 @@
 		header('Location:connect.php');
         exit();
 	}
-	else
-	{
-        // If a title and a content is defined by post (a new page creation request)
-		if (isset($_POST["title"]) && !empty($_POST["title"]) && isset($_POST["content"]) && !empty($_POST["content"]) )
-		{
-			createPage($entityManager, $_POST["title"], $_POST["content"]);
-            echo "page sucessfully created.";
-			$page = defaultPage();
-		}
-        // If a id is defined by get (page request)
-		elseif (isset($_GET["id"]) && !empty($_GET["id"]))
-		{
-			$page = getPage($entityManager, $_GET["id"]);
-		}
-        // if list is set by get (pages list request)
-		elseif (isset($_GET["list"]))
-		{
-			$page = listPages($entityManager);
-		}
-        // if edit is set and has a page id
-        elseif (isset($_GET["edit"]) && !empty($_GET["edit"]))
-        {
-            $page = editPage($entityManager, $_GET["edit"]);
-        }
-        // if delete is defined by get (page deletion request)
-		elseif (isset($_GET["delete"]) && !empty($_GET["delete"]))
-		{
-			deletePage($entityManager, $_GET["delete"]);
-            echo "page sucessfully deleted.";
-			$page = defaultPage();
-		}
-        // if nothing is defined, page is a default page
-		else
-		{
 
-			$page = defaultPage();
-		}
-	}
+    // If a title and a content is defined by post (a new page creation request)
+    if (isset($_POST["title"]) && !empty($_POST["title"]) && isset($_POST["content"]) && !empty($_POST["content"]) )
+    {
+        createPage($entityManager, $_POST["title"], $_POST["content"]);
+        echo "page sucessfully created.";
+        $page = defaultPage();
+    }
+    // If a id is defined by get (page request)
+    elseif (isset($_GET["id"]) && !empty($_GET["id"]))
+    {
+        $page = getPage($entityManager, $_GET["id"]);
+    }
+    // if list is set by get (pages list request)
+    elseif (isset($_GET["list"]))
+    {
+        $page = listPages($entityManager);
+    }
+    // if edit is set and has a page id
+    elseif (isset($_GET["edit"]) && !empty($_GET["edit"]))
+    {
+        $page = editPage($entityManager, $_GET["edit"]);
+    }
+    // if delete is defined by get (page deletion request)
+    elseif (isset($_GET["delete"]) && !empty($_GET["delete"]))
+    {
+        deletePage($entityManager, $_GET["delete"]);
+        echo "page sucessfully deleted.";
+        $page = defaultPage();
+    }
+    // if nothing is defined, page is a default page
+    else
+    {
+        $page = defaultPage();
+    }
+
 	$title = $page[0];
 	$content = $page[1];
 ?>
