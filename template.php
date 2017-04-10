c279c4fb8c8ba7c1af6dba619fe4d1df5222a52c
Killian P <killian.perche@etu.udamail.fr>
2017-04-10 17:40:07 +0200

    Ajout de l'importation du footer (pas encore défini)

--- template.php
+++ template.php
@@ -1,5 +1,6 @@
 <?php
     
     function getHeader() { return file_get_contents("template_header.html"); }
+    //function getFooter() { return file_get_contents("template_footer.html"); }
     
 ?>
